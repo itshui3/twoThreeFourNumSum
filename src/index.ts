c1cb0a275f1c4056fb14d0db897ffd7a60759dd0
Hui <xuhuizhu369@gmail.com>
2021-04-23 19:53:53 -0700

    adds solutions for brute force and optimal two num sum as well as cases

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('hi');
+
+// two num sum algos
+import { 
+    bf_twoNumberSum, 
+    opt_twoNumberSum, 
+    twoNumSumCases } from './twoNumSum';
+// three num sum algos
+
+// four num sum algos
+
+
+// two num sum applications
+for (let i = 0; i < twoNumSumCases.length; i++) {
+    const bruteForceSolution = bf_twoNumberSum(
+        twoNumSumCases[i].array, 
+        twoNumSumCases[i].targetSum);
+    const optimalSolution = opt_twoNumberSum(
+        twoNumSumCases[i].array, 
+        twoNumSumCases[i].targetSum);
+
+    console.log(`
+    brute force solution: [${bruteForceSolution}]
+    optimal solution: [${optimalSolution+''}]
+    expected: [${twoNumSumCases[i].expects+''}]
+    `);
+}
